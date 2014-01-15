381d479d5185eff0ab03b364cb66487da1573410
Michael Rooney <mrooney.github@rowk.com>
2014-01-14 23:48:31 -0600

    finally something performant (canvas 2d)

--- js/game.js
+++ js/game.js
@@ -16,31 +16,94 @@ var main = function() {
     var canvas = document.getElementById("canvas");
     canvas.width = width * tilesize;
     canvas.height = height * tilesize;
-    var context = canvas.getContext("2d");
+    var ctx = canvas.getContext("2d");
+
+    var buffer = document.createElement("canvas");
+    buffer.width = width * tilesize;
+    buffer.height = height * tilesize;
 
     var low = noiseLevel(25);
     var mid = noiseLevel(5);
     var high = noiseLevel(3);
 
-    for (var x=0; x < width; x++) {
-        for (var y=0; y < height; y++) {
-            var n = low(x,y) + high(x,y) * .1;
-            var n2 = mid(x,y);
-            var n3 = high(x,y);
-            if (n < .6) { // ocean
-                style = 'blue';
-                if (n2 > .6) { style = '#6495ED'; }
-            } else if (n < .7) { // sand
-                style = '#FEF0C9';
-                //if (n2 > .75) { style = '#D2691E'; }
-            } else { // grass
-                style = '#32CD32';
-                if (n3 > .7) { style = 'gray'; }
-            }
-            //var style = "rgb(" + n + "," + n + "," + n + ")";
-            context.fillStyle = style;
-            context.fillRect(x*tilesize, y*tilesize, tilesize, tilesize);
-        } 
+    var state = {
+        pressed: {},
     }
+
+    function init() {
+        document.onkeydown = function(e) {
+            var key = String.fromCharCode(e.keyCode);
+            state.pressed[key] = true;
+        }
+
+        document.onkeyup = function(e)
+        {
+            var keyCode = ('which' in e) ? e.which : e.keyCode;
+            var key = String.fromCharCode(keyCode);
+            state.pressed[key] = false;
+        }
+    }
+
+    var imgdata;
+    renderChunk();
+    function render(delta) {
+        ctx.drawImage(buffer, 0, 0);
+    }
+
+    function renderChunk(cx, cy) {
+        var ctx = buffer.getContext("2d");
+        for (var x=0; x < width; x++) {
+            for (var y=0; y < height; y++) {
+                var n = low(x,y) + high(x,y) * .1;
+                var n2 = mid(x,y);
+                var n3 = high(x,y);
+                if (n < .6) { // ocean
+                    style = 'blue';
+                    if (n2 > .6) { style = '#6495ED'; }
+                } else if (n < .7) { // sand
+                    style = '#FEF0C9';
+                    //if (n2 > .75) { style = '#D2691E'; }
+                } else { // grass
+                    style = '#32CD32';
+                    if (n3 > .7) { style = 'gray'; }
+                }
+                //var style = "rgb(" + n + "," + n + "," + n + ")";
+                ctx.fillStyle = style;
+                ctx.fillRect(x*tilesize, y*tilesize, tilesize, tilesize);
+            }
+        }
+        imgdata = ctx.getImageData(0, 0, width*tilesize, height*tilesize);
+    }
+    
+    function update(delta) {
+        if (state.pressed["W"] === true) {
+            ctx.translate(0, 1);
+        }
+        if (state.pressed["S"] === true) {
+            ctx.translate(0, -1);
+        }
+        if (state.pressed["A"] === true) {
+            ctx.translate(1, 0);
+        }
+        if (state.pressed["D"] === true) {
+            ctx.translate(-1, 0);
+        }
+    }
+
+    var last = null;
+    function animate(timestamp) {
+        var delta = 0;
+        if (last !== null) {
+            delta = timestamp - last;
+        }
+        requestAnimationFrame(animate);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        update(delta);
+        render(delta);
+        last = timestamp;
+    }
+
+    init();
+    requestAnimationFrame(animate);
 }
 main();
