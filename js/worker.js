6dccb34cd01b1a7ee3ea296f71fd2764132d981e
Michael Rooney <mrooney.github@rowk.com>
2014-01-22 00:13:06 -0600

    minor cleanup

--- js/worker.js
+++ js/worker.js
@@ -6,24 +6,21 @@ onmessage = function(e) {
     var cx = e.data.cx;
     var cy = e.data.cy;
     var imgData = e.data.imgData;
+    var data = imgData.data;
 
-    var chunkStyles = new Array(chunkSize*chunkSize);
-    var x, y, style;
-
+    var chunkstyles = new Array(chunkSize*chunkSize);
     var start = new Date().getTime();
 
-    for (x=0; x < chunkSize; x++) {
-        for (y=0; y < chunkSize; y++) {
-            chunkStyles[y*chunkSize+x] = constants.get_block_style(cx, cy, x, y);
+    for (var x=0; x < chunkSize; x++) {
+        for (var y=0; y < chunkSize; y++) {
+            chunkstyles[y*chunkSize+x] = constants.get_block_style(cx, cy, x, y);
         }
     }
 
-
-    var data = imgData.data;
     for (y = 0; y < constants.chunkspan; y++) {
         for (x = 0; x < constants.chunkspan; x++) {
             offset = (y * constants.chunkspan + x) * 4;
-            rgb = chunkStyles[Math.floor(y/tileSize) * chunkSize + Math.floor(x/tileSize)];
+            rgb = chunkstyles[Math.floor(y/tileSize) * chunkSize + Math.floor(x/tileSize)];
             data[offset] = rgb[0];
             data[offset + 1] = rgb[1];
             data[offset + 2] = rgb[2];
